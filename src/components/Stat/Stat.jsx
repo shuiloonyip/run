532b48808386655920c659162012479c71871fa3
shuiloonyip <97403751+shuiloonyip@users.noreply.github.com>
2023-07-12 17:26:35 +0100

    Add background

--- src/components/Stat/Stat.jsx
+++ src/components/Stat/Stat.jsx
@@ -3,7 +3,7 @@ import styles from "./Stat.module.css";
 function Stat({ stat, label }) {
   return (
     <div className={styles.stat}>
-      <div>{stat}</div>
+      <div className={styles.number}>{stat}</div>
       <div className={styles.label}>{label}</div>
     </div>
   );
